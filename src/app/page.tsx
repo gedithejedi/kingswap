fdc9dd67f834eac43c093064670be0bc3989e9b4
“DaeunYoon” <daa3230@gmail.com>
2024-03-16 14:18:59 +0000

    feat: add characters on bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
   }
 
   return (
-    <main className="flex flex-col min-h-screen bg-bg-gray text-white">
+    <main className="min-w-screen min-h-screen bg-bg-gray text-white">
       {tokenPopupPayload && <TokenSelectPopup 
         {...tokenPopupPayload}
         chainId={currentChainOrDefaultChain}
@@ -77,71 +77,75 @@ export default function Home() {
         close={() => setTokenPopupPayload(undefined)}
       />}
 
-      <div className="w-full flex justify-end py-3 px-4">
-        <div className="w-60">
-          <DynamicWidget variant='modal' />
+      <div className="w-full h-screen relative flex flex-col">
+        <div className="w-full flex justify-end py-3 px-4">
+          <div className="w-60">
+            <DynamicWidget variant='modal' />
+          </div>
         </div>
-      </div>
 
-      <div>
-        <h1 className={`text-5xl tracking-wider font-bold text-center uppercase ${londrina.className}`}>Kingswap</h1>
-      </div>
+        <div>
+          <h1 className={`text-5xl tracking-wider font-bold text-center uppercase ${londrina.className}`}>Kingswap</h1>
+        </div>
 
-      <div className="flex flex-col items-center mt-10 gap-9">
-        <div className="flex flex-col gap-y-5 items-center md:w-[80%] max-w-[600px]">
-          <div className="flex flex-col items-center w-full">
-            <div className="relative w-full">
-              <Image src="/star.png" alt="star" width={70} height={100} className="absolute top-[-94px] left-4 z-0" />
-              <Image src="/star.png" alt="star" width={40} height={50} className="absolute top-[-54px] right-4 z-0" />
-              <PriceInput
-                chain={currentChainOrDefaultChain}
-                amountToSwap={amountToSwap}
-                setAmountToSwap={setAmountToSwap}
-                selectedToken={tokenToSwapFrom}
-                setTokenPopupPayload={setTokenPopupPayload}
-                disabledTokens={tokenToSwapTo ? [tokenToSwapTo] : []}
-                setSelectedToken={setTokenToSwapFrom}
-                disabled={!isChainSupported}
-               />
-              <div className="absolute bottom-[-16px] w-full flex items-center justify-center">
-                <div
-                  className={`bg-gray-light border-bg-gray w-10 h-10 border-4 rounded-lg flex items-center justify-center ${isChainSupported ? "cursor-pointer hover:bg-primary": "cursor-not-allowed"}`}
-                  onClick={() => {
-                    const temp = tokenToSwapFrom;
-                    setTokenToSwapFrom(tokenToSwapTo);
-                    setTokenToSwapTo(temp);
-                  }}
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3" />
-                  </svg>
+        <div className="flex flex-col items-center mt-10 gap-9">
+          <div className="flex flex-col gap-y-5 items-center md:w-[80%] max-w-[600px]">
+            <div className="flex flex-col items-center w-full">
+              <div className="relative w-full">
+                <Image src="/star.png" alt="star" width={70} height={100} className="absolute top-[-94px] left-4 z-0" />
+                <Image src="/star.png" alt="star" width={40} height={50} className="absolute top-[-54px] right-4 z-0" />
+                <PriceInput
+                  chain={currentChainOrDefaultChain}
+                  amountToSwap={amountToSwap}
+                  setAmountToSwap={setAmountToSwap}
+                  selectedToken={tokenToSwapFrom}
+                  setTokenPopupPayload={setTokenPopupPayload}
+                  disabledTokens={tokenToSwapTo ? [tokenToSwapTo] : []}
+                  setSelectedToken={setTokenToSwapFrom}
+                  disabled={!isChainSupported}
+                />
+                <div className="absolute bottom-[-16px] w-full flex items-center justify-center">
+                  <div
+                    className={`bg-gray-light border-bg-gray w-10 h-10 border-4 rounded-lg flex items-center justify-center ${isChainSupported ? "cursor-pointer hover:bg-primary": "cursor-not-allowed"}`}
+                    onClick={() => {
+                      const temp = tokenToSwapFrom;
+                      setTokenToSwapFrom(tokenToSwapTo);
+                      setTokenToSwapTo(temp);
+                    }}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3" />
+                    </svg>
+                  </div>
                 </div>
               </div>
+              <PriceInput
+                chain={currentChainOrDefaultChain}
+                isNumberInputDisabled={true}
+                amountToSwap={amountToSwap}
+                setAmountToSwap={setAmountToSwap}
+                selectedToken={tokenToSwapTo}
+                setTokenPopupPayload={setTokenPopupPayload}
+                disabledTokens={tokenToSwapFrom ? [tokenToSwapFrom] : []}
+                setSelectedToken={setTokenToSwapTo}
+                disabled={!isChainSupported}
+              />
             </div>
-            <PriceInput
-              chain={currentChainOrDefaultChain}
-              isNumberInputDisabled={true}
-              amountToSwap={amountToSwap}
-              setAmountToSwap={setAmountToSwap}
-              selectedToken={tokenToSwapTo}
-              setTokenPopupPayload={setTokenPopupPayload}
-              disabledTokens={tokenToSwapFrom ? [tokenToSwapFrom] : []}
-              setSelectedToken={setTokenToSwapTo}
-              disabled={!isChainSupported}
-            />
+            <Button 
+              type="primary" 
+              className="w-full text-lg py-3" 
+              onClick={permitToken}
+              disabled={isButtonDisabled || !doesUserHaveEnoughBalance}
+            >
+              {!isConnected ? 'Please connect wallet': isButtonDisabled ? "Permit" : `Permit ${amountToSwap} ${tokenToSwapFrom?.symbol ?? 'ETH'}`}
+            </Button>
+            {!doesUserHaveEnoughBalance && <p className="text-red-400 text-start w-full">
+              It looks like you don&apos;t have enough balance to swap {amountToSwap} {tokenToSwapFrom?.symbol ?? 'ETH'}
+            </p>}
           </div>
-          <Button 
-            type="primary" 
-            className="w-full text-lg py-3" 
-            onClick={permitToken}
-            disabled={isButtonDisabled || !doesUserHaveEnoughBalance}
-          >
-            {!isConnected ? 'Please connect wallet': isButtonDisabled ? "Permit" : `Permit ${amountToSwap} ${tokenToSwapFrom?.symbol ?? 'ETH'}`}
-          </Button>
-          {!doesUserHaveEnoughBalance && <p className="text-red-400 text-start w-full">
-            It looks like you don&apos;t have enough balance to swap {amountToSwap} {tokenToSwapFrom?.symbol ?? 'ETH'}
-          </p>}
         </div>
+        <Image src="/king.png" alt="king" width={150} height={200} className="absolute bottom-0 left-24" />
+        <Image src="/cat.png" alt="cat" width={150} height={200} className="absolute bottom-0 right-24" />
       </div>
     </main>
   );
