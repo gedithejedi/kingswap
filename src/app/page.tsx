9b4042df11ccb56390fe547f8866ff671accfe79
“DaeunYoon” <daa3230@gmail.com>
2024-03-16 04:08:33 +0000

    add name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,10 @@ export default function Home() {
         </div>
       </div>
 
+      <div>
+        <h1 className="text-3xl font-bold text-center uppercase">Kingswap</h1>
+      </div>
+
       <div className="flex flex-col items-center mt-10 gap-9">
         <div className="flex flex-col gap-y-5 items-center md:w-[80%] max-w-[600px]">
           <div className="flex flex-col items-center w-full">
