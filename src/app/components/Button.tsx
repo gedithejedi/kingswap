7c2178be2885758b61fac1d173bb2c3bf00b89ec
gedithejedi <33606955+gedithejedi@users.noreply.github.com>
2024-03-16 20:45:24 +0000

    fix: indentation

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,33 +1,33 @@
 import { Spinner } from "./Spinner"
 
 type ButtonProps = {
-    type?: 'primary' | 'secondary'
-    className?: string
-    children: React.ReactNode
-    onClick: () => void
-    disabled?: boolean
-    isLoading?: boolean
+  type?: 'primary' | 'secondary'
+  className?: string
+  children: React.ReactNode
+  onClick: () => void
+  disabled?: boolean
+  isLoading?: boolean
 }
 
 export default function Button({
-    type,
-    className,
-    children,
-    onClick,
-    disabled,
-    isLoading,
+  type,
+  className,
+  children,
+  onClick,
+  disabled,
+  isLoading,
 }: ButtonProps) {
-    const colorStyle = type === 'primary' ? `bg-primary ${disabled ? '' : 'hover:bg-primary-focus'}` : `bg-gray-dark ${disabled ? '' : 'hover:bg-gray-darker'}`
-    const disabledStyle = disabled || isLoading ? 'cursor-not-allowed opacity-50' : ''
+  const colorStyle = type === 'primary' ? `bg-primary ${disabled ? '' : 'hover:bg-primary-focus'}` : `bg-gray-dark ${disabled ? '' : 'hover:bg-gray-darker'}`
+  const disabledStyle = disabled || isLoading ? 'cursor-not-allowed opacity-50' : ''
 
-    return (
-        <button
-            className={`${colorStyle} flex gap-2 items-center justify-center py-2 text-white rounded-lg ${disabledStyle} ${className}`}
-            onClick={onClick}
-            disabled={isLoading || disabled}
-        >
-            {isLoading && <Spinner />}
-            {children}
-        </button>
-    )
+  return (
+    <button
+      className={`${colorStyle} flex gap-2 items-center justify-center py-2 text-white rounded-lg ${disabledStyle} ${className}`}
+      onClick={onClick}
+      disabled={isLoading || disabled}
+    >
+      {isLoading && <Spinner />}
+      {children}
+    </button>
+  )
 }
--- src/app/components/Transfer.tsx
+++ src/app/components/Transfer.tsx
@@ -7,79 +7,79 @@ import TokenSelectPopup from "./TokenSelectPopup";
 import Button from "./Button";
 
 type TransferProps = {
-    isChainSupported: boolean;
-    currentChainOrDefaultChain: Chains;
-    address?: `0x${string}`;
-    permitToken: () => void;
-    isLoading: boolean;
+  isChainSupported: boolean;
+  currentChainOrDefaultChain: Chains;
+  address?: `0x${string}`;
+  permitToken: () => void;
+  isLoading: boolean;
 }
 
 export default function Transfer({
-    isChainSupported,
-    currentChainOrDefaultChain,
-    address,
-    permitToken,
-    isLoading
+  isChainSupported,
+  currentChainOrDefaultChain,
+  address,
+  permitToken,
+  isLoading
 }: TransferProps) {
-    const [amount, setAmount] = useState<string | undefined>();
-    const [receiver, setReceiver] = useState<string | undefined>();
-    const [selectedToken, setSelectedToken] = useState<TokenConfig | undefined>();
-    const [tokenPopupPayload, setTokenPopupPayload] = useState<{
-        selectedToken?: TokenConfig,
-        disabledTokens: TokenConfig[],
-        onSelect: Dispatch<SetStateAction<undefined | TokenConfig>>,
-    }>();
-    const isButtonDisabled = !isChainSupported || amount === undefined;
+  const [amount, setAmount] = useState<string | undefined>();
+  const [receiver, setReceiver] = useState<string | undefined>();
+  const [selectedToken, setSelectedToken] = useState<TokenConfig | undefined>();
+  const [tokenPopupPayload, setTokenPopupPayload] = useState<{
+    selectedToken?: TokenConfig,
+    disabledTokens: TokenConfig[],
+    onSelect: Dispatch<SetStateAction<undefined | TokenConfig>>,
+  }>();
+  const isButtonDisabled = !isChainSupported || amount === undefined;
 
-    return (
-        <div>
-            {tokenPopupPayload && <TokenSelectPopup
-                {...tokenPopupPayload}
-                chainId={currentChainOrDefaultChain}
-                isOpen={!!tokenPopupPayload}
-                close={() => setTokenPopupPayload(undefined)}
-            />}
+  return (
+    <div>
+      {tokenPopupPayload && <TokenSelectPopup
+        {...tokenPopupPayload}
+        chainId={currentChainOrDefaultChain}
+        isOpen={!!tokenPopupPayload}
+        close={() => setTokenPopupPayload(undefined)}
+      />}
 
-            <div className="flex flex-col w-full">
-                <PriceInput
-                    chain={currentChainOrDefaultChain}
-                    amount={amount}
-                    setAmount={setAmount}
-                    selectedToken={selectedToken}
-                    setTokenPopupPayload={setTokenPopupPayload}
-                    disabledTokens={[]}
-                    setSelectedToken={setSelectedToken}
-                    disabled={!isChainSupported}
-                    label="Amount to send"
-                />
-                <div className="bg-gray-light w-full p-4 px-5 rounded-lg">
-                    <label className="flex flex-col gap-y-3">
-                        <span className="text-sm font-semibold">Recipient</span>
-                        <input
-                            type="text"
-                            value={receiver}
-                            onChange={e => setReceiver(e.target.value)}
-                            className="bg-gray-light w-full focus:outline-none text-xl "
-                            placeholder="0x23dfc90..."
-                        />
-                    </label>
-                </div>
-
-                <Button
-                    isLoading={isLoading}
-                    type="primary"
-                    className="w-full text-lg py-3 mt-5 font-semibold"
-                    onClick={permitToken}
-                    disabled={isButtonDisabled}
-                >
-                    {!address
-                        ? 'Please connect wallet'
-                        : isButtonDisabled
-                            ? "Permit"
-                            : `Permit ${amount} ${selectedToken?.symbol ?? 'ETH'}`
-                    }
-                </Button>
-            </div>
+      <div className="flex flex-col w-full">
+        <PriceInput
+          chain={currentChainOrDefaultChain}
+          amount={amount}
+          setAmount={setAmount}
+          selectedToken={selectedToken}
+          setTokenPopupPayload={setTokenPopupPayload}
+          disabledTokens={[]}
+          setSelectedToken={setSelectedToken}
+          disabled={!isChainSupported}
+          label="Amount to send"
+        />
+        <div className="bg-gray-light w-full p-4 px-5 rounded-lg">
+          <label className="flex flex-col gap-y-3">
+            <span className="text-sm font-semibold">Recipient</span>
+            <input
+              type="text"
+              value={receiver}
+              onChange={e => setReceiver(e.target.value)}
+              className="bg-gray-light w-full focus:outline-none text-xl "
+              placeholder="0x23dfc90..."
+            />
+          </label>
         </div>
-    )
+
+        <Button
+          isLoading={isLoading}
+          type="primary"
+          className="w-full text-lg py-3 mt-5 font-semibold"
+          onClick={permitToken}
+          disabled={isButtonDisabled}
+        >
+          {!address
+            ? 'Please connect wallet'
+            : isButtonDisabled
+              ? "Permit"
+              : `Permit ${amount} ${selectedToken?.symbol ?? 'ETH'}`
+          }
+        </Button>
+      </div>
+    </div>
+  )
 }
