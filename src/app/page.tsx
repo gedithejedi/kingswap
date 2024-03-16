16394d9d0a56dfdc98fb50fe141723748e5a86f4
“DaeunYoon” <daa3230@gmail.com>
2024-03-16 15:51:41 +0000

    feat: add menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,11 @@ import { Dispatch, SetStateAction, useEffect, useMemo, useRef, useState } from "
 import type { TokenConfig } from "@/helpers/types";
 import { getChainOrDefaultChain, isSupportedChain } from '@/helpers/network'
 import { Londrina_Solid } from 'next/font/google'
-import { getBalance } from "@/lib/wallet";
 
 import Image from "next/image";
 import TokenSelectPopup from "./components/TokenSelectPopup";
 import Button from "./components/Button";
-import PriceInput from "./components/PriceInput";
+import Swap from "./components/Swap";
 
 const londrina = Londrina_Solid({ 
   weight: ["300", "400"],
@@ -28,29 +27,10 @@ export default function Home() {
     disabledTokens: TokenConfig[],
     onSelect: Dispatch<SetStateAction<undefined | TokenConfig>>,
   }>();
+  const [selectedMenu, setSelectedMenu] = useState<string>('swap'); 
   const chainId = chain?.id;
   const currentChainOrDefaultChain = useMemo(() => getChainOrDefaultChain(chainId), [chainId]);
   const isChainSupported = useMemo(() => isSupportedChain(chainId), [chainId]);
-  const isButtonDisabled = !isChainSupported || amountToSwap === undefined;
-
-  const userBalance = useRef<number>(0);
-  useEffect(() => {
-    async function checkBalance() {
-      if(!address || !tokenToSwapFrom || !chainId) {
-        userBalance.current = 0;
-        return;
-      };
-
-      userBalance.current = Number(await getBalance(address, tokenToSwapFrom, chainId));
-      return ;
-    }
-    checkBalance();
-  }, [chainId, tokenToSwapFrom, amountToSwap, address]);
-
-  const doesUserHaveEnoughBalance = useMemo(() => {
-    if(!tokenToSwapFrom || !amountToSwap) return true;
-    return userBalance.current >= parseFloat(amountToSwap.replace(/,/g, ''));
-  },[userBalance, amountToSwap, tokenToSwapFrom]);
   
   const permitToken = async () => {
     if(!isChainSupported) {
@@ -71,7 +51,7 @@ export default function Home() {
         close={() => setTokenPopupPayload(undefined)}
       />}
 
-      <div className="w-full h-screen relative flex flex-col">
+      <div className="w-full h-screen relative flex flex-col items-center">
         <div className="w-full flex justify-end py-3 px-4">
           <div className="w-60">
             <DynamicWidget variant='modal' />
@@ -82,60 +62,43 @@ export default function Home() {
           <h1 className={`text-5xl tracking-wider font-bold text-center uppercase ${londrina.className}`}>Kingswap</h1>
         </div>
 
-        <div className="flex flex-col items-center mt-10 gap-9">
-          <div className="flex flex-col gap-y-5 items-center md:w-[80%] max-w-[600px]">
-            <div className="flex flex-col items-center w-full">
-              <div className="relative w-full">
-                <Image src="/star.png" alt="star" width={70} height={100} className="absolute top-[-94px] left-4 z-0" />
-                <Image src="/star.png" alt="star" width={40} height={50} className="absolute top-[-54px] right-4 z-0" />
-                <PriceInput
-                  chain={currentChainOrDefaultChain}
-                  amountToSwap={amountToSwap}
-                  setAmountToSwap={setAmountToSwap}
-                  selectedToken={tokenToSwapFrom}
-                  setTokenPopupPayload={setTokenPopupPayload}
-                  disabledTokens={tokenToSwapTo ? [tokenToSwapTo] : []}
-                  setSelectedToken={setTokenToSwapFrom}
-                  disabled={!isChainSupported}
-                />
-                <div className="absolute bottom-[-16px] w-full flex items-center justify-center">
-                  <div
-                    className={`bg-gray-light border-bg-gray w-10 h-10 border-4 rounded-lg flex items-center justify-center ${isChainSupported ? "cursor-pointer hover:bg-primary": "cursor-not-allowed"}`}
-                    onClick={() => {
-                      const temp = tokenToSwapFrom;
-                      setTokenToSwapFrom(tokenToSwapTo);
-                      setTokenToSwapTo(temp);
-                    }}
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3" />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-              <PriceInput
-                chain={currentChainOrDefaultChain}
-                isNumberInputDisabled={true}
-                amountToSwap={amountToSwap}
-                setAmountToSwap={setAmountToSwap}
-                selectedToken={tokenToSwapTo}
-                setTokenPopupPayload={setTokenPopupPayload}
-                disabledTokens={tokenToSwapFrom ? [tokenToSwapFrom] : []}
-                setSelectedToken={setTokenToSwapTo}
-                disabled={!isChainSupported}
-              />
+        <div className="md:w-[80%] max-w-[600px]">
+          <div className="relative flex flex-col items-center mt-10 gap-9 rounded-xl py-4 px-2 border-2">
+            <Image src="/star.png" alt="star" width={70} height={100} className="absolute top-[-96px] left-4 z-0" />
+            <Image src="/star.png" alt="star" width={40} height={50} className="absolute top-[-56px] right-4 z-0" />
+
+            <div className="flex w-[150px] justify-between gap-x-2">
+              <Button 
+                onClick={() => setSelectedMenu('swap')} 
+                disabled={selectedMenu === 'swap'}
+                className="px-4"
+              >
+                Swap
+              </Button>
+              <Button 
+                onClick={() => setSelectedMenu('transfer')}  
+                disabled={selectedMenu === 'transfer'}
+                className="px-4"
+              >
+                Transfer
+              </Button>
             </div>
-            <Button 
-              type="primary" 
-              className="w-full text-lg py-3" 
-              onClick={permitToken}
-              disabled={isButtonDisabled || !doesUserHaveEnoughBalance}
-            >
-              {!isConnected ? 'Please connect wallet': isButtonDisabled ? "Permit" : `Permit ${amountToSwap} ${tokenToSwapFrom?.symbol ?? 'ETH'}`}
-            </Button>
-            {!doesUserHaveEnoughBalance && <p className="text-red-400 text-start w-full">
-              It looks like you don&apos;t have enough balance to swap {amountToSwap} {tokenToSwapFrom?.symbol ?? 'ETH'}
-            </p>}
+
+            {selectedMenu === 'swap' && 
+              <Swap 
+                isChainSupported={isChainSupported} 
+                currentChainOrDefaultChain={currentChainOrDefaultChain}
+                address={address}
+                chainId={chainId} 
+              />
+            }
+
+            {selectedMenu === 'transfer' && 
+              <div>
+                Coming soon
+              </div>
+            }
+            
           </div>
         </div>
         <Image src="/king.png" alt="king" width={150} height={200} className="absolute bottom-0 left-24" />
